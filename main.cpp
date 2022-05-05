a9bb63d5bebe9fc8ea891010b7c9fd33d9eac01e
masster-sedla <103938626+masster-sedla@users.noreply.github.com>
2022-05-05 13:56:40 -0700

    first exs 2

--- main.cpp
+++ main.cpp
@@ -1,23 +1,21 @@
 #include <iostream>
+#include <string>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
-void PrintWorryCount(const vector<bool>& is_nervous) {
-    // подсчитываем количество элементов в векторе is_nervous, равных true
-    /*int worry_count = 0;
-    for (bool status : is_nervous) {
-        if (status) {
-            ++worry_count;
-        }
-    }*/
-    cout << count(is_nervous.begin(), is_nervous.end(),false)/*worry_count*/ << endl;
-
+bool pop(string xs){
+    if(xs[0]!=' ')
+        return ((xs[0]=='a') || (xs[0]=='A'));
+}
+int CountStartsWithA(const vector<string>& xs) {
+    // посчитайте число строк, начинающихся на букву A
+    return count_if(xs.begin(), xs.end(), pop);
 }
 
 int main() {
-    // не меняйте содержимое функции main
-    PrintWorryCount({true, true, false, true});
-    return 0;
+    // не меняйте тело main
+    cout << CountStartsWithA({"And"s, "another"s, "one"s, "gone"s, "another"s, "one"s "bites"s, "the"s, "dust"s});
+		return 0;
 }
